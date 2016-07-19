dc9fb1adcc93736b163e0acb13e6414db46e351e
Wolden, Alexander <Alexander.Wolden@nike.com>
2016-07-18 18:16:19 -0700

    initial

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var table = require('text-table');
+
+const header = ['*Package*', '*Version*', '*FixedIn*', '*Advisory*'];
+
+module.exports = function(err, data, pkgPath) {
+    console.log(err, data, pkgPath)
+    if (err) {
+        return 'Debug output: ' + JSON.stringify(Buffer.isBuffer(data) ? data.toString() : data) + '\n' + err;
+    }
+
+    if (!data.length) {
+        return `(+) Zero Vulnerabilities Found`;
+    }
+
+    return format(data);
+};
+
+function format(data) {
+    const tableData = [header];
+    const rows = data.map((advisory) => {
+        return [
+            advisory.module,
+            advisory.vulnerable_versions,
+            advisory.patched_versions,
+            `<${advisory.advisory}|${advisory.id}>`,
+            `\n_${advisory.path.join('>')}_`
+        ];
+    });
+
+    const fullTable = tableData.concat(rows);
+    return table(fullTable, {
+        hsep: ' ',
+        stringLength: (string) => {
+          string = string.replace(/\*(\w*)\*/g, '$1');
+          return string.length;
+        }
+    });
+}
